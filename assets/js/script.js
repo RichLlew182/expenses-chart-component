5670897a5fa4fc9ba45811aef9875e99dc231b23
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-04-30 16:21:12 +0100

    added fetch request for the data json file, looped through it to add up all the amounts and then set this as the total in the html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
 console.log('Hello World!')
+
+const totalExpenses = document.getElementById('total-expenses')
+
+fetch('data.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+
+    let total = 0;
+
+    for (let i = 0; i < data.length; i++) {
+      let amount = data[i].amount;
+      total += amount;
+    }
+
+    console.log(total);
+
+    totalExpenses.innerText = '$' + total
+
+  })
+  .catch(error => {
+    console.error('There was a problem fetching the data:', error);
+  });
