ad69f6d7320e3cfeae8083c53126c93876cec3fd
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-04-30 17:26:28 +0100

    working on getting today's date

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@ const days = document.querySelectorAll('.day');
 const dayAmounts = document.querySelectorAll('.day-amount');
 const dayExpenses = document.querySelectorAll('.day-expenses');
 
+
+  const daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  const currentDate = new Date();
+  const dayIndex = currentDate.getDay();
+  let today = daysOfWeek[dayIndex];
+
+
+console.log(today)
+
+
+
 function fetchData() {
   fetch('data.json')
     .then(response => {
@@ -14,6 +25,11 @@ function fetchData() {
       console.log(data);
 
       for (let i = 0; i < days.length; i++) {
+
+        if (today === data[i].day) {
+          console.log('Today is ' + data[i].day)
+        }
+
         if (days[i].id === data[i].day) {
           dayAmounts[i].innerText = '$' + data[i].amount;
           dayExpenses[i].style.height = Math.floor(data[i].amount) * 2.9 + 'px'
