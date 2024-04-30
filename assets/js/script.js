67f48c302f8afdd61f3add82ad64694407252edc
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-04-30 16:52:40 +0100

    looped through the data and then set the height of the expenses based on the amounts in the json file, using a multiplication to get as close to the mockup as possible

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
-console.log('Hello World!')
-
 const days = document.querySelectorAll('.day');
-const dayAmounts = document.querySelectorAll('.day-amount')
+const dayAmounts = document.querySelectorAll('.day-amount');
+const dayExpenses = document.querySelectorAll('.day-expenses');
 
-fetch('data.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-
-    for (let i = 0; i < days.length; i++) {
-      if (days[i].id === data[i].day) {
-        dayAmounts[i].innerText = '$' + data[i].amount
+function fetchData() {
+  fetch('data.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-    }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
 
+      for (let i = 0; i < days.length; i++) {
+        if (days[i].id === data[i].day) {
+          dayAmounts[i].innerText = '$' + data[i].amount;
+          dayExpenses[i].style.height = Math.floor(data[i].amount) * 2.9 + 'px'
+        }
+      }
 
-  })
-  .catch(error => {
-    console.error('There was a problem fetching the data:', error);
-  });
+    })
+    .catch(error => {
+      console.error('There was a problem fetching the data:', error);
+    });
+
+}
+
+setTimeout(fetchData,500)
